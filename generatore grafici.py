8ceb5c6c17ad7a982a1b63ae5b29244939999d34
gabrielecoding <74988836+gabrielecoding@users.noreply.github.com>
2021-01-27 23:13:40 +0100

    Add files via upload

--- generatore grafici.py
+++ generatore grafici.py
@@ -1,19 +1,42 @@
+#lavoro di Ippolito Gabriele, Avallone Enrico e Lastrucci Davide
+
+#Il codice serve ad aprire o a generare dei grafici.
+#Nel caso si voglia generare un grafico casuale, basterà seguire le istruzioni dell'interfaccia.
+#Mentre nel caso si voglia aprire un grafico proprio, bisognerà inserire la directory del file + il nome del file + formato del file
+
+import tkinter as tk
+import csv
 from tkinter import * 
 import numpy as np
 import matplotlib.pyplot as plt
-from guizero import App, PushButton, Text
-import csv
 from random import randint
-import tkinter as tk
-from PIL import Image
-from PIL import ImageTk
-from pygame.locals import *
-import pygame, sys
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+from PIL import Image, ImageTk
+from matplotlib.offsetbox import TextArea, DrawingArea, OffsetImage, AnnotationBbox
+import matplotlib.image as mpimg
 
+
+def getGraphRoot():
+
+    rispostagrafico = richiesta_grafico.get()
+    
+    width = 530
+    height = 350
+
+    load = Image.open(rispostagrafico)
+    load = load.resize((width,height), Image.ANTIALIAS)
+    render = ImageTk.PhotoImage(load)
+    
+    img = Label(image=render)
+    img.image = render
+    
+    riquadro.create_window(450, 400, window=img)    
+    root.mainloop()
+        
 def genera_grafico():
-    root = Tk() 
-    root.title('Generatore di grafici')
-    root.geometry('400x400') 
     f = open("dati.txt", 'w')
     dati = ""
     for riga in range(100):
@@ -45,71 +68,34 @@ def genera_grafico():
     print ("Y: ",coordY)
     print(type(coordX))
     print(type(coordY))
-    def graph():
-        plt.scatter(coordX, coordY)
-        plt.show()
-    c = Button(root,text="premimi per avere il grafico", command=graph)
-    c.pack()
-    root.mainloop()
 
-def carica_file_csv():
-    ro = Tk()
-    ro.title("carica file csv")
-    riquadro = tk.Canvas(ro, width = 400, height = 400)
-    riquadro.pack()
+    plt.scatter(coordX, coordY)
+    plt.show()
 
-    richiesta_grafico = Entry(ro)
-    riquadro.create_window(200, 140, window=richiesta_grafico)
-
-    scritta=Label(ro, text="inserisci il nome del file")
-    riquadro.create_window(200,110, window=scritta)
-
-    def file_csv():
-        rispostagrafico = richiesta_grafico.get()
-    
-        with open(rispostagrafico, 'r') as file:
-            reader = csv.reader(file)
-            for row in reader:
-                print(row)
-
-        print("")
-        print("")
-
-        with open(rispostagrafico, 'r') as file:
-            csv_file = csv.DictReader(file)
-            for row in csv_file:
-                print(dict(row))
-
-    my_button = tk.Button(ro, text="clicca per confermare", command=file_csv)
-
-    riquadro.create_window(200, 180, window=my_button)
-
-    ro.mainloop()
-
-#richiesta_grafico = Entry(root)
-#riquadro.create_window(200, 140, window=richiesta_grafico)
-
-#scritta=Label(root, text="inserisci il nome del file")
-#riquadro.create_window(200,110, window=scritta)
-
-def getGraphRoot():
-    rispostagrafico = input("inserire directory del file...")
-    
-    pygame.init()
-    display_width = (640)
-    display_height = (480)
-    graph=pygame.image.load(rispostagrafico)
-    screen=pygame.display.set_mode((display_width, display_height))
-    screen.blit(graph, (1,1))
-    pygame.display.update()
-
-app = App(title="Carica grafici", width=250, bg="light cyan", height=200, layout="grid")
-button1 = PushButton(app, command=carica_file_csv,  text="clicca qui per caricare un file csv", grid=[400,50])
-button2 = PushButton(app, command=getGraphRoot,  text="clicca qui per caricare un grafico", grid=[400,150])
-button3 = PushButton(app, command=genera_grafico,  text="clicca qui per generare un grafico", grid=[400,250])
-button4 = PushButton(app, command=app.destroy, text="Clicca qui per chiudere la scheda", grid=[400,350])
-app.display()
+root = Tk()
+riquadro = tk.Canvas(root, width = 900, height = 600, bg="dark sea green")
+riquadro.pack()
 
 
 
-#LAVORO DI: Gabriele Ippolito, Davide Lastrucci, Enrico avallone
+scritta=Label(root, text="inserisci la directory del file da aprire", bg= "dark sea green")
+riquadro.create_window(450,110, window=scritta)
+
+#button 1 permette all'utente di generare il grafico.
+button1 = tk.Button(root, text="genera un grafico", command=genera_grafico, bg= "NavajoWhite2")
+riquadro.create_window(55, 15, window=button1)
+
+#button 2 permette all'utente di chiudere tutte le finestre aperte.
+button2 = tk.Button(root, text="chiudi scheda", command=root.destroy, bg="NavajoWhite2")
+riquadro.create_window(860, 15, window=button2)
+
+#button 3 permette all'utente di caricare un file dal proprio computer.
+button3 = tk.Button(root, text="clicca per aprire il file", command=getGraphRoot, bg= "NavajoWhite2")
+riquadro.create_window(450, 180, window=button3)
+
+richiesta_grafico = Entry(root)
+riquadro.create_window(450, 140, window=richiesta_grafico)
+
+
+root.mainloop()
+
