8f04ac467a6df3b0dfd4379a18f6d87e2a56a673
gabrielecoding <74988836+gabrielecoding@users.noreply.github.com>
2021-05-19 00:49:56 +0200

    Add files via upload

--- Haiku/generatore haiku.py
+++ Haiku/generatore haiku.py
@@ -1,26 +1,84 @@
 from random import randint as rd
 import os
 import csv
+import tkinter as tk
+import csv
+from tkinter import *
+from tkinter import font 
 
-os.chdir(r"C:\Users\ippol\OneDrive\Desktop\Haiku") 
+#os.chdir("file") 
 
-f = open("cinq1.txt")
-line = csv.reader(f)
+a = open("Downloads\Haiku\cinq1.txt")
+line = csv.reader(a)
 v1 = list(line)
 
-g = open("sette.txt")
-line = csv.reader(g)
+b = open("Downloads\Haiku\sette.txt")
+line = csv.reader(b)
 v2 = list(line)
 
-h = open("cinq2.txt")
-line = csv.reader(h)
+c = open("Downloads\Haiku\cinq2.txt")
+line = csv.reader(c)
 v3 = list(line)
 
-verso1 = v1[rd(0,5)]
-verso2 = v2[rd(0,5)]
-verso3 = v3[rd(0,7)]
+
+root = Tk()
+riquadro = tk.Canvas(root, width = 600, height = 300, bg="dark sea green")
+root.title("GENERATORE DI HAIKU")
+riquadro.pack()
+
+riquadro2 = tk.Canvas(root, width = 200, height = 120, bg="dark sea green")
+riquadro2.pack()
+riquadro.create_window(300,140, window=riquadro2)
+
+scritta1 = riquadro2.create_text(100,30, font= ("times", 14, "italic"))
+scritta2 = riquadro2.create_text(100,60, font= ("times", 14, "italic"))
+scritta3 = riquadro2.create_text(100,90, font= ("times", 14, "italic"))
+
+def genera_haiku(Side=BOTTOM):
+    
+    verso1 = v1[rd(0,15)]
+    verso2 = v2[rd(0,16)]
+    verso3 = v3[rd(0,17)]
 
 
-print(*verso1)
-print(*verso2)
-print(*verso3)
+    riquadro2.itemconfig(scritta1, text=str(*verso1))
+    riquadro2.itemconfig(scritta2, text=str(*verso2))
+    riquadro2.itemconfig(scritta3, text=str(*verso3))
+
+
+    #riquadro.place(x=70, y=55)
+
+def cambia_rigo1():
+    verso1 = v1[rd(0,15)]
+    riquadro2.itemconfig(scritta1, text=str(*verso1))
+    
+
+def cambia_rigo2():
+    verso2 = v2[rd(0,16)]
+    riquadro2.itemconfig(scritta2, text=str(*verso2))
+    
+
+def cambia_rigo3():
+    verso3 = v3[rd(0,17)]
+    riquadro2.itemconfig(scritta3, text=str(*verso3))
+    
+
+button1 = tk.Button(root, text="chiudi scheda", command=root.destroy, bg="NavajoWhite2")
+riquadro.create_window(560, 15, window=button1)
+
+button2 = tk.Button(root, text="Genera Haiku", command=genera_haiku, font= ("monospace", 12, "bold"), bg= "NavajoWhite2")
+riquadro.create_window(300, 50, window=button2)
+
+
+buttonr1 = tk.Button(root, text="cambia", command=cambia_rigo1, font= ("NavajoWhite2", 7), bg="NavajoWhite2")
+riquadro.create_window(185, 110, window=buttonr1)
+
+buttonr2 = tk.Button(root, text="cambia", command=cambia_rigo2, font= ("NavajoWhite2", 7), bg="NavajoWhite2")
+riquadro.create_window(185, 140, window=buttonr2)
+
+buttonr3 = tk.Button(root, text="cambia", command=cambia_rigo3, font= ("NavajoWhite2", 7), bg="NavajoWhite2")
+riquadro.create_window(185, 170, window=buttonr3)
+
+
+
+root.mainloop()
