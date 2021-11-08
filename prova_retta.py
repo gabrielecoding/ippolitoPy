5333624c9259efc89dad0d8a017ea8c227774d37
gabrielecoding <74988836+gabrielecoding@users.noreply.github.com>
2021-11-08 07:54:48 +0100

    Update and rename oop/file_retta.py to prova_retta.py

--- prova_retta.py
+++ prova_retta.py
@@ -1,55 +1,55 @@
 class retta:
 
     def __init__(self, __a, __b, __c):
-        self.a = int(__a)
-        self.b = int(__b)
-        self.c = int(__c)
-        self.xy = (0, 0)
+        self._a = float(__a)
+        self._b = float(__b)
+        self._c = float(__c)
+        self._xy = (0, 0)
         self.lista_punti = []
 
     
     
     def implicita(self):
-        if self.b == 0:
-            return f"---------------------------\n\n equazione forma implicita: \n {self.a}x + {self.c} = 0 \n (y= 0) "
-        elif self. a == 0:
-            return f"---------------------------\n\n equazione forma implicita: \n {self.b}y + {self.c} = 0 \n (x= 0) "
+        if self._b == 0:
+            return f"---------------------------\n\n equazione forma implicita: \n {self._a}x + {self._c} = 0 \n (y= 0) "
+        elif self._a == 0:
+            return f"---------------------------\n\n equazione forma implicita: \n {self._b}y + {self._c} = 0 \n (x= 0) "
         else:
-            return f"---------------------------\n\n equazione forma implicita: \n {self.a}x + {self.b}y + {self.c} = 0 "
+            return f"---------------------------\n\n equazione forma implicita: \n {self._a}x + {self._b}y + {self._c} = 0 "
 
     def esplicita(self):
-        if self.b == 0:
-            return f"\n\n equazione forma esplicita: \n x = {- self.c/ self.a} \n (y= 0) \n\n ---------------------------"
-        elif self.a == 0:
-            return f"\n\n equazione forma esplicita: \n y = {- self.c/ self.b} \n (x= 0) \n\n ---------------------------"
+        if self._b == 0:
+            return f"\n\n equazione forma esplicita: \n x = {- self._c/ self._a} \n (y= 0) \n\n ---------------------------"
+        elif self._a == 0:
+            return f"\n\n equazione forma esplicita: \n y = {- self._c/ self._b} \n (x= 0) \n\n ---------------------------"
         else:
-            return f"\n\n equazione forma esplicita: \n y = {- self.a / self.b}x + {- self.c/ self.b} \n\n ---------------------------"
+            return f"\n\n equazione forma esplicita: \n y = {- self._a / self._b}x + {- self._c/ self._b} \n\n ---------------------------"
 
     def coefficiente_angolare(self):
-        if self.b == 0:
+        if self._b == 0:
             return f"\ncoefficiente angolare: \n\n la retta è parallela all'asse delle y, il coefficiente angolare non è definito in alcun modo... \n\n ---------------------------"
         else:
-            return f"\ncoefficiente angolare: \n\n m= {- self.a / self.b} \n\n ---------------------------"
+            return f"\ncoefficiente angolare: \n\n m= {- self._a / self._b} \n\n ---------------------------"
 
     def ricavo_punti(self):
 
         inizio = - (1/2) * int(n)
         fine = (1/2) * int(n)
 
-        if self.a == 0:
+        if self._a == 0:
             for i in range(int(inizio), int(fine)):
                 x = i
-            self.lista_punti.append((x, - self.c/ self.b))
+            self.lista_punti.append((x, - self._c/ self._b))
         
-        elif self.b == 0:
+        elif self._b == 0:
             for i in range(int(inizio), int(fine)):
                 y = i
-            self.lista_punti.append((- self.c/ self.a, y))
+            self.lista_punti.append((- self._c/ self._a, y))
 
         else:    
             for i in range(int(inizio), int(fine)):
                 x = i
-                self.lista_punti.append ((x, (- self.a * x - self.c) / self.b))
+                self.lista_punti.append ((x, (- self._a * x - self._c) / self._b))
         
         print(self.lista_punti)
 
@@ -89,3 +89,8 @@ while True:
         else:
             print("errore, non hai inserito uno dei due valori")
             break
+        
+
+
+
+
