a3c58afd378ec582fb1826d6f0976a40dd2c5970
gabrielecoding <74988836+gabrielecoding@users.noreply.github.com>
2022-01-07 12:24:52 +0100

    Update gioco.py

--- gioco_prototipo/gioco.py
+++ gioco_prototipo/gioco.py
@@ -6,7 +6,7 @@ import os
 from pygame.constants import K_0, K_DOWN, K_LEFT, K_RIGHT, K_SPACE, K_UP, KEYDOWN, KEYUP, K_a, K_d, K_s, K_w
 from pygame.time import Clock
 
-os.chdir ("/Users/gabriele/Downloads/immagini")
+os.chdir ("Downloads/gioco_prototipo")
 
 pygame.init()
 random.seed()
